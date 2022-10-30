24ca6c58dea13a0dc3b9bacc936718ec3ae683ec
Virendra <official.vksingh01@gmail.com>
2022-10-30 19:34:50 +0530

    final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+console.log('Virendra');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
